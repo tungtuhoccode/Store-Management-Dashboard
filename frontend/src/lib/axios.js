598095a44364c04a16faf944f0f6adcddf35ffeb
tung <thanhtungtran112233@gmail.com>
2025-05-05 21:17:42 -0400

    Fix axios config

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios"
 
 const BackendURL = import.meta.env.VITE_BACKEND_URL;
 const axiosInstance = axios.create({
-    baseURL: "http://10.0.0.135:3000/api" || "http://localhost:3000/api",
+    baseURL: BackendURL || "http://localhost:3000/api",
     withCredentials: true // send cookies to the server //
 })
 
