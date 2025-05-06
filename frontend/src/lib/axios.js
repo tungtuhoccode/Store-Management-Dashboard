7c418098ad9289cea10d1f2eef406a1d41f3997f
tung <thanhtungtran112233@gmail.com>
2025-05-05 21:09:47 -0400

    commit

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,7 +4,7 @@ import axios from "axios"
 const BackendURL = import.meta.env.VITE_BACKEND_URL;
 const axiosInstance = axios.create({
     baseURL: "http://10.0.0.135:3000/api" || "http://localhost:3000/api",
-    withCredentials: true // send cookies to the server
+    withCredentials: true // send cookies to the server //
 })
 
 export default axiosInstance;
