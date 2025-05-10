d88dfd8d3947aa8cd53dd21a734fe79994e121f0
tung <thanhtungtran112233@gmail.com>
2025-05-09 20:34:01 -0400

    feat: Fetch data from back-end using React-Query

--- frontend/src/pages/Utils/filterHelper.js
+++ frontend/src/pages/Utils/filterHelper.js
@@ -1,5 +1,22 @@
 //generate unique values from a table of data 
 export const generateUniqueValues  = (data, columnId) => {
+    console.log(data)
     const columnValues = data.map(row => row[columnId])
     return ([... new Set(columnValues)])
 }
+
+function optimizeImageUrl(url) {
+  if (url.includes("res.cloudinary.com")) {
+    return url.replace(/\/upload\//, "/upload/c_scale,w_150/");
+  }
+  if (url.includes("cloutcloset.com")) {
+    return url.replace(/width=\d+/, "width=100");
+  }
+  return url;
+}
+
+// Given your existing `data` array, produce a new array with optimized image URLs:
+export const optimizedData = (data) => ( data.map(item => ({
+  ...item,
+  image: optimizeImageUrl(item.image),
+})));
