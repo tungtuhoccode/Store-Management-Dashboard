ffdb5f9ad609bd52b4e2ff9f2de90ad2a5511102
tung <thanhtungtran112233@gmail.com>
2025-05-11 15:30:38 -0400

    feat: Add get single order with products route

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,13 +1,14 @@
 import express from "express"
 import { adminRoute, protectRoute } from "../middleware/authMiddleware.js";
 import { applyCoupon, getCoupon } from "../controllers/couponController.js";
-import {getOrders } from "../controllers/orderController.js"
+import {getOrders, getSingleOrder } from "../controllers/orderController.js"
 
 
 const router = express.Router();
 
 
 router.get("/", protectRoute, adminRoute, getOrders );
+router.get("/:id", protectRoute, getSingleOrder );
 //late add add coupon route for admin
 
 
