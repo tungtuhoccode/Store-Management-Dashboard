13cba112569f7172578d634ab73fc55d9336805e
tung <thanhtungtran112233@gmail.com>
2025-05-17 15:48:29 -0400

    feat: Showing latest order by default

--- frontend/src/pages/AdminPage/AdminOrderPage/AdminOrderManagementPage.jsx
+++ frontend/src/pages/AdminPage/AdminOrderPage/AdminOrderManagementPage.jsx
@@ -98,81 +98,6 @@ import {
 import FilterDialog from "./components/FilterDialog.jsx";
 import OrderPageHeader from "./components/OrderPageHeader";
 
-const data = [
-  {
-    order_id: "1135e96f-702b-4bc9-acb7-31a9d43c00f4",
-    order_number: "10000002",
-    user_email: "vunguyen@gmail.com",
-    total_amount: "276.83",
-    create_at: "2025-05-05T10:28:12.449Z",
-    item_count: "3",
-    fulfillment_status: "pending",
-  },
-  {
-    order_id: "cccb9a65-66b3-463f-9741-cae95a4a4683",
-    order_number: "10000006",
-    user_email: "tungadmin@gmail.com",
-    total_amount: "69.80",
-    create_at: "2025-05-11T22:08:12.338Z",
-    item_count: "3",
-    fulfillment_status: "pending",
-  },
-  {
-    order_id: "e7955cae-e83c-4639-8c20-bc1b0f23510c",
-    order_number: "10000007",
-    user_email: "tungadmin@gmail.com",
-    total_amount: "19.94",
-    create_at: "2025-05-11T22:09:17.212Z",
-    item_count: "1",
-    fulfillment_status: "pending",
-  },
-  {
-    order_id: "b0462ebe-d0e2-4ce0-9027-c2132e6c68de",
-    order_number: "10000003",
-    user_email: "test2@gmail.com",
-    total_amount: "101.69",
-    create_at: "2025-05-05T12:35:18.303Z",
-    item_count: "1",
-    fulfillment_status: "in progress",
-  },
-  {
-    order_id: "a88f3dfc-186b-4f27-ab55-9baa0421af26",
-    order_number: "10000004",
-    user_email: "test2@gmail.com",
-    total_amount: "355.24",
-    create_at: "2025-05-06T14:52:02.455Z",
-    item_count: "5",
-    fulfillment_status: "shipped",
-  },
-  {
-    order_id: "216d4034-9c35-4dde-be15-03af4960acc8",
-    order_number: "10000001",
-    user_email: "test2@gmail.com",
-    total_amount: "300.56",
-    create_at: "2025-05-05T06:34:27.455Z",
-    item_count: "3",
-    fulfillment_status: "delivered",
-  },
-  {
-    order_id: "c152ee43-8126-44cd-82e2-9c659220b108",
-    order_number: "10000008",
-    user_email: "tunguser@gmail.com",
-    total_amount: "74.00",
-    create_at: "2025-05-11T23:29:42.436Z",
-    item_count: "3",
-    fulfillment_status: "cancelled",
-  },
-  {
-    order_id: "157cbabb-29b8-4e88-a084-a7284345b7a3",
-    order_number: "10000005",
-    user_email: "tungadmin@gmail.com",
-    total_amount: "51.43",
-    create_at: "2025-05-11T22:04:37.833Z",
-    item_count: "2",
-    fulfillment_status: "shipped",
-  },
-].sort((a, b) => b.order_number.localeCompare(a.order_number));
-
 const columns = [
   {
     accessorKey: "order_number",
@@ -412,7 +337,9 @@ export default function AdminOrderManagementPage() {
   const queryClient = useQueryClient();
   const orderQuery = useQuery({queryKey: ["orders"], queryFn: fetchOrders, refetchInterval: 10000})
 
-  const [sorting, setSorting] = React.useState([]);
+  const [sorting, setSorting] = React.useState([
+    { id: 'create_at', desc: true }, // false = ascending
+  ]);
   const [columnFilters, setColumnFilters] = React.useState([
     { id: "fulfillment_status", value: [] },
     { id: "user_email", value: [] },
