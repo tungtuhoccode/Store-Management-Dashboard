7efaaf8ae56f82a177d4e10b0994d8c10071556f
tung <thanhtungtran112233@gmail.com>
2025-05-18 18:57:48 -0400

    build: Change folder structure, add dockerfile for backend

--- rundev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-osascript <<EOF
-tell application "Terminal"
-    do script "cd $(pwd)/frontend && npx vite --host"
-    delay 1
-    do script "cd $(pwd)/backend && npm run dev"
-end tell
-EOF
